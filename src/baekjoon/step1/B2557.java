99e7abb282bc763d7c9562910495ad7dee0ef876
JeongJinUng <jinung.dev@gmail.com>
2021-12-24 02:22:01 +0900

    [baekjoon] Hello World!를 출력하시오.

---
+++ src/baekjoon/step1/B2557.java
@@ -0,0 +1,13 @@
+package baekjoon.step1;
+
+/**
+ * @since 2021.12.24
+ * @author DevJeong
+ * [문제]
+ * Hello World!를 출력하시오
+ */
+public class B2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
