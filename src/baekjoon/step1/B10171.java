aa23fdd4aa36a90f1cae6c2056b8d6cfcbd0d8c4
JeongJinUng <jinung.dev@gmail.com>
2021-12-28 23:43:45 +0900

    [beakjoon] 고양이

---
+++ src/baekjoon/step1/B10171.java
@@ -0,0 +1,18 @@
+package baekjoon.step1;
+
+/**
+ * @since 2021.12.24
+ * @author DevJeong
+ * [문제]
+ * 아래 예제와 같이 고양이를 출력하시오.
+ */
+public class B10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\ ");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
+
