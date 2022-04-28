00eceb3aaede269e9e6049a778c71fb8d3081dd8
Jeong Jin Ung <jinung.dev@gmail.com>
2022-04-29 00:14:39 +0900

    [beakjoon] 나머지

---
+++ src/baekjoon/step1/B10430.java
@@ -0,0 +1,23 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/**
+ * @date 2022.04.28
+ * @author DevJeong
+ */
+public class B10430 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
