3a45f9f4116b9892861846eead32627962be91b8
Jeong Jin Ung <jinung.dev@gmail.com>
2022-04-28 23:18:24 +0900

    [beakjoon] ??!

---
+++ src/baekjoon/step1/B10926.java
@@ -0,0 +1,20 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/**
+ * @date 2022.04.28
+ * @author DevJeong
+ */
+public class B10926 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.next();
+        scanner.close();
+
+        System.out.println(id + "??!");
+
+    }
+}
