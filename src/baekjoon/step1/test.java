092e69527f8d3f627e585227f508c2760b1b577d
Jeong Jin Ung <jinung.dev@gmail.com>
2022-05-02 02:17:42 +0900

    fixed untracked files

---
+++ src/baekjoon/step1/test.java
@@ -0,0 +1,18 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/**
+ * @date 2022.04.29
+ * @author DevJeong
+ */
+public class test {
+
+    public static void main(String[] args) {
+       int[] test;
+       test = new int[] {1,2,3,4,5,6,7,8,9};
+
+        System.out.println(test);
+
+    }
+}
